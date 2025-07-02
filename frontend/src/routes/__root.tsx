0909ce9f2f00cc9e402822ce48bb19e22a9d801f
Polilla23 <leandro.vizzolini@hotmail.com>
2025-07-02 17:35:35 -0300

    added navbar (excluding routes)

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,11 +1,24 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { Navbar } from '../components/ui/navbar'
+import { useRouter } from '@tanstack/react-router'
 
-export const Route = createRootRoute({
-  component: () => (
+function RootComponent() {
+  const router = useRouter()
+  const pathname = router.state.location.pathname
+
+  // Oculta el navbar en cualquier ruta que empiece por /user/
+  const hideNavbar = pathname.startsWith('/user/')
+
+  return (
     <>
+      {!hideNavbar && <Navbar />}
       <Outlet />
       <TanStackRouterDevtools />
     </>
-  ),
+  )
+}
+
+export const Route = createRootRoute({
+  component: RootComponent,
 })
